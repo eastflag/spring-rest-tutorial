b9a4b0ecae2a1bcbfa0a9cd2a9f16f0f727c4718
eastflag <dklee98@>
2021-03-31 14:02:06 +0900

    image 업로드 및 보기 api

---
+++ src/main/java/com/eastflag/fullstack/controller/ImageController.java
@@ -0,0 +1,44 @@
+package com.eastflag.fullstack.controller;
+
+import com.eastflag.fullstack.domain.BoardVO;
+import com.eastflag.fullstack.domain.ImageVO;
+import com.eastflag.fullstack.persistence.ImageMapper;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+    private final ImageMapper imageMapper;
+
+    @PostMapping("/upload")
+    public Integer handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+
+        ImageVO imageVO = new ImageVO();
+        imageVO.setMimetype(file.getContentType());
+        imageVO.setOriginal_name(file.getOriginalFilename());
+        imageVO.setData(file.getBytes());
+        imageMapper.insertBoard(imageVO);
+
+        return imageVO.getId();
+    }
+
+    @GetMapping("/view/{id}")
+    public ResponseEntity<byte[]> findOne(@PathVariable int id) {
+        ImageVO imageVO = imageMapper.findOneImage(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", imageVO.getMimetype());
+        headers.add("Content-Length", String.valueOf(imageVO.getData().length));
+
+        return new ResponseEntity<byte[]>(imageVO.getData(), headers, HttpStatus.OK);
+    }
+}

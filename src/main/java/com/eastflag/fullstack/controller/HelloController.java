7878a9f1bde6c5f25c1f3411273f7d7f4509e4ff
eastflag <dklee98@>
2021-04-02 18:35:45 +0900

    hello 함수

--- src/main/java/com/eastflag/fullstack/controller/HelloController.java
+++ src/main/java/com/eastflag/fullstack/controller/HelloController.java
@@ -1,4 +1,38 @@
 package com.eastflag.fullstack.controller;
 
+import com.eastflag.fullstack.domain.ResultVO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello test";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "Hello GET test";
+    }
+
+    @GetMapping("/hello3")
+    public String hello3(@RequestParam("name") String name) {
+        return "Hello" + name;
+    }
+
+    @GetMapping("/hello32/{name}")
+    public String hello32(@PathVariable String name) {
+        return "Hello" + name;
+    }
+
+    @PostMapping("/hello5")
+    public ResultVO hello5(@RequestParam("name") String name) {
+        ResultVO result = new ResultVO(0, name);
+        return result;
+    }
+
+    @PostMapping("/hello6")
+    public ResultVO hello6(@RequestBody ResultVO result) {
+        return result;
+    }
 }
--- src/main/java/com/eastflag/fullstack/domain/ResultVO.java
+++ src/main/java/com/eastflag/fullstack/domain/ResultVO.java
@@ -2,7 +2,9 @@ package com.eastflag.fullstack.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class ResultVO {
